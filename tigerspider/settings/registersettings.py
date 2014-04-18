96113a5aa89b9cee457d0a12f307c190e90ec64d
wuyadong <wuyadong@tigerknows.com>
2014-04-18 17:40:08 +0800

    regist some spider

--- tigerspider/settings/registersettings.py
+++ tigerspider/settings/registersettings.py
@@ -10,7 +10,12 @@ __author__ = ['"wuyadong" <wuyadong@tigerknows.com>']
 
 spiders = [
     'spiders.com228.spider.Com228Spider',
-    'spiders.intro1.spider.Intro1Spider'
+    'spiders.intro1.spider.Intro1Spider',
+    'spiders.ctrip.spider.CtripSpider',
+    'spiders.nuomi.spider.NuomiSpider',
+    'spiders.lvyoubaidu.spider.LvYouBaiDuSpider',
+    'spiders.lvyoubaidutag.spider.LvYouBaiDuTagSpider',
+    'spiders.lvyoudaodao.spider.LvYouDaoDaoSpider',
 ]
 
 schedules = [
